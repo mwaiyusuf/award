6b190b875e5a5c87e21d53bd0eab8d12116af0b4
mohammed yusuf <mwaigalo5@gmail.com>
2018-09-15 19:47:57 +0300

    t

---
+++ js/custom.js
@@ -0,0 +1,49 @@
+//function temp(){
+//    var name='Mohammed';
+//    alert(name);
+//}
+//temp()
+//function logArgs(...args) {
+//    for (let [index, elem] of args.entries()) {
+//        const message = index + '. ' + elem;
+//        console.log(message);
+//    }
+//}
+//logArgs('Hello', 'everyone');
+
+// Output:
+// 0. Hello
+// 1. everyone
+//let number=[1,2,3,4,5];
+//function sumUp(toAdd){
+//    result=0;
+//    for(let i=0;i<toAdd.length;i++){
+//     result += toAdd[i];   
+//    }
+//    return result;
+//}
+//console.log(sumUp(numbers);
+//function ArrayF(){
+//    let number=[]
+//    for(let i=0;i<number.length;i++){
+//        alert(number);
+//    }
+//}
+//ArrayF()
+//let name=prompt("Enter your name");;
+////document.write("HEllo " + name);
+//let  fn = () =>{
+//    let a=3;
+//    let b=7;
+//    return a+b;
+//}
+//console.log(fn());
+//let NotWorkingFunction = (a = 10, b) => {
+// return a + b;
+//}
+//NotWorkingFunction(20); 
+let name="Mohammed"
+let x=44;
+let y=22;
+console.log(`${name} you have earn ${2 * (x + y)} dolars`);
+console.log(name.includes("o"));
